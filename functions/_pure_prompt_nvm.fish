bb033298876d2ffa8325b33155d7853ed288e483
Charlie Karniol <charliekarniol@gmail.com>
2020-01-17 20:15:50 +0200

    only show nvm prompt if node in path

--- functions/_pure_prompt_nvm.fish
+++ functions/_pure_prompt_nvm.fish
@@ -6,18 +6,12 @@ function _pure_prompt_nvm \
     end
 
     set --local is_git_repository (command git rev-parse --is-inside-work-tree 2>/dev/null)
-    if test -n "$is_git_repository"
+    set --local has_node (which node)
+    if test -n "$is_git_repository" -a -n "$has_node"
         set --local node_prefix "node.js"
         set --local node_color (_pure_set_color $pure_color_node)
         set --local node_prompt "$node_color$node_prefix"
-
-        set --local has_node (which node)
-        if test -n "$has_node"
-            set node_prompt $node_prompt (_pure_prompt_node_version)(_pure_prompt_node_dirty)
-        else
-            set node_prompt $node_prompt "n/a"
-        end
-
+        set node_prompt $node_prompt (_pure_prompt_node_version)(_pure_prompt_node_dirty)
         echo "$node_prompt"
     end
 end
